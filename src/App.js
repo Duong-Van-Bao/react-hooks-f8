0c4b41cc0745afd2e3fb8e51e29034e0dad60e56
Duong_Van_Bao <duongbao9109@gmail.com>
2022-09-08 07:44:20 +0700

    todo list 100%

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import { useState } from "react";
-
 // const gifts = ["CPU i9", "RAM 32GB RGB", "RGB Keyboard"];
-const courses = [
-  { id: 1, name: "HTML, CSS" },
-  { id: 2, name: "Javascript" },
-  { id: 3, name: "ReactJS" },
-];
+// const courses = [
+//   { id: 1, name: "HTML, CSS" },
+//   { id: 2, name: "Javascript" },
+//   { id: 3, name: "ReactJS" },
+// ];
 
 function App() {
-  const [checked, setChecked] = useState();
+  // const [checked, setChecked] = useState([]);
 
   // const [gift, setGift] = useState();
 
@@ -21,39 +19,49 @@ function App() {
   // const [name, setName] = useState("");
   // const [email, setEmail] = useState("");
 
-  const handleSubmit = () => {
-    // CALL API
-    // console.log({
-    //   name,
-    //   email,
-    // });
-    console.log({ id: checked });
-  };
+  // const handleCheck = (id) => {
+  //   setChecked((prev) => {
+  //     const isChecked = checked.includes(id);
+  //     if (isChecked) {
+  //       return checked.filter((item) => item !== id);
+  //     } else {
+  //       return [...prev, id];
+  //     }
+  //   });
+  // };
+
+  // const handleSubmit = () => {
+  // CALL API
+  // console.log({
+  //   name,
+  //   email,
+  // });
+  //   console.log({ ids: checked });
+  // };
 
   return (
-    <div className="App" style={{ padding: 32 }}>
-      {courses.map((course) => (
-        <div key={course.id}>
-          {/* <input
+    //   {courses.map((course) => (
+    //     <div key={course.id}>
+    /* <input
             type="radio"
             checked={checked === course.id}
             onChange={() => setChecked(course.id)}
-          /> */}
-          <input
+          /> */
+    /* <input
             type="checkbox"
-            checked={checked === course.id}
-            onChange={() => setChecked(course.id)}
+            checked={checked.includes(course.id)}
+            onChange={() => handleCheck(course.id)}
           />
           {course.name}
         </div>
       ))}
-      <button onClick={handleSubmit}>Register</button>
-      {/* <input value={name} onChange={(e) => setName(e.target.value)} />
-      <input value={email} onChange={(e) => setEmail(e.target.value)} /> */}
-      {/* <button onClick={() => setName("Nguyen van B")}></button> */}
-      {/* <h1>{ gift || 'Chưa có phần thưởng'}</h1> */}
-      {/* <button onclick={randomGift}></button> */}
-    </div>
+      <button onClick={handleSubmit}>Register</button> */
+    /* <input value={name} onChange={(e) => setName(e.target.value)} />
+      <input value={email} onChange={(e) => setEmail(e.target.value)} /> */
+    /* <button onClick={() => setName("Nguyen van B")}></button> */
+    /* <h1>{ gift || 'Chưa có phần thưởng'}</h1> */
+    /* <button onclick={randomGift}></button> */
+    <div style={{ padding: 32 }}></div>
   );
 }
 
